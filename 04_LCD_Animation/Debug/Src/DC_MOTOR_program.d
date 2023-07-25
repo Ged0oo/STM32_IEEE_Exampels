a7312cda3c6215752c73ec36b22bfc0a097520ff
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-25 15:18:48 +0300

    Add files via upload

---
+++ 04_LCD_Animation/Debug/Src/DC_MOTOR_program.d
@@ -0,0 +1,12 @@
+Src/DC_MOTOR_program.o: ../Src/DC_MOTOR_program.c ../Inc/STD_TYPES.h \
+ ../Inc/BIT_MATH.h ../Inc/STD_TYPES.h ../Inc/BIT_MATH.h \
+ ../Inc/DC_MOTOR_interface.h ../Inc/GPIO_interface.h \
+ ../Inc/GPIO_private.h ../Inc/GPIO_config.h
+../Inc/STD_TYPES.h:
+../Inc/BIT_MATH.h:
+../Inc/STD_TYPES.h:
+../Inc/BIT_MATH.h:
+../Inc/DC_MOTOR_interface.h:
+../Inc/GPIO_interface.h:
+../Inc/GPIO_private.h:
+../Inc/GPIO_config.h:
